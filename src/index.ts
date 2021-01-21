061a3e0127256e5d456c14473566dc8e53ffe925
Mark Tyneway <mark.tyneway@gmail.com>
2021-01-20 18:49:48 -0800

    fixes: some npm packaging stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 export * from './dump-generation/environment'
 export * from './dump-generation/state-dump'
-
 import { DumpEnvironment } from './dump-generation/environment'
 import test00_BasicEnqueue from './dump-scripts/00-basic-enqueue'
+
+const fs = require('fs')
+const path = require('path')
+const DIR = path.join(__dirname, '..', '..', 'dumps')
+
+const files = fs.readdirSync(DIR)
+
+for (const name of files) {
+  const p = path.join(DIR, name)
+  const file = require(p)
+  module.exports[name] = file
+}
 
 export const tests: {
   name: string
