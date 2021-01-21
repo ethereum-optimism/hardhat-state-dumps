0de6b1263093e626e1a5e0c07b749cfdff79525d
Mark Tyneway <mark.tyneway@gmail.com>
2021-01-21 10:44:30 -0800

    dumps: regenerate using correct mnemonic

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,25 @@
 import { HardhatUserConfig } from "hardhat/types"
+import { defaultPath } from '@ethersproject/hdnode'
 
 // Plugins
 import '@nomiclabs/hardhat-ethers'
 
+let mnemonic = process.env.MNEMONIC;
+if (typeof mnemonic === 'undefined') {
+  mnemonic = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
+}
+
 const config: HardhatUserConfig = {
-  solidity: "0.7.3"
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+        path: defaultPath,
+        count: 5,
+      }
+    }
+  }
 }
 
 export default config
